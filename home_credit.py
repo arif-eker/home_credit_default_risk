a9f9e0e31c7ab1f03e267b084b7b0d7697cd4e9d
Arif <eng.arifeker@gmail.com>
2020-12-05 11:01:44 +0300

    temel kütüphaneler eklendi

--- home_credit.py
+++ home_credit.py
@@ -2,26 +2,7 @@
 
 import pandas as pd
 import numpy as np
-
+import scripts.helper_functions as hlp
 
 pd.pandas.set_option("display.max_columns", None)
-
-
-
-# reading data
-
-df = pd.read_csv("datasets/bureau.csv")
-
-df.head()
-
-df.groupby("SK_ID_CURR")["SK_ID_BUREAU"].count().head()
-
-df["AMT_ANNUITY"].quantile(0.995)
-df["AMT_ANNUITY"].mean()
-df["AMT_ANNUITY"].median()
-
-df["AMT_ANNUITY"].describe()
-
-df.shape
-
-df["CREDIT_TYPE"].unique()
+pd.set_option("display.float_format", lambda x: '%.2f' % x)
